d220e95f00d72c951358d811acd33d2d37ec156f
Jahidul Islam <jahid.info97@gmail.com>
2024-02-27 23:20:55 +0600

    fix(tailwind): tailwind proper fix
    Tailwind was not working using className, due to poor setup, now fixed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="bg-red-500 justify-center items-center flex-1">
       <Text>Open up App.tsx to start working on your app!</Text>
-
       <StatusBar style="auto" />
     </View>
   );
